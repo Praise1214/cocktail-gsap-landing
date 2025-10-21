41d6c9308fd4d30773664989a49fe428c90c60ab
Praise1214 <agapepraiz@gmail.com>
2025-10-21 09:38:46 +0100

    animate the right and left leaf using gsap timeline and scrolltrigger

--- MOJITO_WEBSITE/src/components/Hero.jsx
+++ MOJITO_WEBSITE/src/components/Hero.jsx
@@ -23,7 +23,7 @@ const Hero = () => {
       duration: 1.8,
       ease: 'expo-out',
       stagger: 0.05,
-    })
+    });
 
     gsap.from(paragraphSplit.lines, {
       opacity:0,
@@ -32,7 +32,18 @@ const Hero = () => {
       ease: 'expo-out',
       stagger: 0.06,
       delay: 1,
+    });
+
+    gsap.timeline({
+      scrollTrigger: {
+        trigger: '#hero',
+        start: 'top top',
+        end: 'bottom top',
+        scrub: true,
+      }
     })
+      .to('.right-leaf', { y:200}, 0 )
+      .to('.left-leaf', {y: -200}, 0)
   }, [])
   return (
     <>
