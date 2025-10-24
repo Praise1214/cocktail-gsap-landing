7072ba64b6b074d47fe5f7e8aa883e29937e2f92
Praise1214 <agapepraiz@gmail.com>
2025-10-24 01:38:57 +0100

    Implement the contact section of the component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+'use client';
 import { allCocktails } from "../../constants/index.js";
 import { useState, useRef } from "react";
 import { useGSAP } from "@gsap/react";
